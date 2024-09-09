8b774828791f03ec459d6a5152fb1a9548c6b3b5
Vitor F. Lins <vflins@live.com>
2024-09-09 16:52:14 -0300

    add logging

--- src/nflogic/cache.py
+++ src/nflogic/cache.py
@@ -1,13 +1,27 @@
 import pickle
+import logging
 from pathlib import Path
 from os import path, makedirs
 
 
 SCRIPT_PATH = path.split(path.realpath(__file__))[0]
 CACHE_PATH = path.join(SCRIPT_PATH, "cache")
-CACHE_TYPES = ["passed", "failed", "keys"]
+LOG_PATH = path.join(SCRIPT_PATH, "log")
+LOG_FILE = path.join(SCRIPT_PATH, "log", f"{__name__}.log")
 
-makedirs(CACHE_PATH, exist_ok=True)
+for directory in [CACHE_PATH, LOG_PATH]:
+    makedirs(directory, exist_ok=True)
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+makedirs(path.split(LOG_FILE)[0], exist_ok=True)
+loghandler = logging.FileHandler(filename=LOG_FILE)
+logformat = logging.Formatter(fmt="%(asctime)s %(levelname)s :: %(message)s")
+loghandler.setFormatter(logformat)
+loghandler.setLevel(logging.INFO)
+log.addHandler(loghandler)
+log.propagate = False
 
 
 class KeyAlreadyProcessedError(Exception):
@@ -26,32 +40,36 @@ class KeyNotFoundError(Exception):
 
 class CacheHandler:
     def __init__(self, cachename: str) -> None:
-        self.cachefile = path.join(CACHE_PATH, cachename + ".cache")
+        self.cachename = cachename
+        self.cachefile = path.join(CACHE_PATH, f"{cachename}.cache")
         self.data = self._load()
 
     def _load(self) -> list:
         Path(self.cachefile).touch(exist_ok=True)
         with open(self.cachefile, "rb") as cache:
             try:
-                return pickle.load(cache)
+                output: list = pickle.load(cache)
             except EOFError:
-                return []
+                output = []
+            return output
 
     def _heal(self) -> None:
-        # TODO: Warn inconsistent sizes
         size_diff = len(self._load()) - len(self.data)
         if size_diff == 0:
             return
+        log.warning(f"Inconsistente cache sizes, starting '_heal' method from {self.cachename}.log")
 
         if size_diff < 0:
+            # recreate file and write contents from memory
             Path(self.cachefile).touch(exist_ok=True)
             with open(self.cachefile, "wb") as cache:
                 pickle.dump(obj=self.data, file=cache)
-            # TODO: Info how many items were restored from memory to cache
+            log.info(f"Restored {abs(size_diff)} items to {self.cachename}.log")
 
         else:
+            # read file contents
             self.data = self._load()
-            # TODO: Info how many items were restored from cache to memory
+            log.info(f"Restored {size_diff} items from {self.cachename}.log")
 
     def add(self, item) -> None:
         if item in self._load():
